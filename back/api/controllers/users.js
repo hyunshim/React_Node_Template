d306675e04f41ce97bac6d4c7b033b34e6d9c36c
Hyun <you@example.com>
2019-05-13 02:28:23 +1000

    Add get user, create user, delete user API calls

--- back/api/controllers/users.js
+++ back/api/controllers/users.js
@@ -1,6 +1,6 @@
 const User = require('../models/user');
 const mongoose = require('mongoose');
-const selectFields = '';
+const selectFields = '_id name';
 
 exports.get_all_users = (req, res, next) => {
     User.find()
@@ -17,6 +17,50 @@ exports.get_all_users = (req, res, next) => {
             res.status(200).json(response);
         })
         .catch(error => {
-            res.status(500).json({ error: error })
+            res.status(500).json({ message: `Unable to GET all users`, error: error })
         });
+}
+
+exports.get_user = (req, res, next) => {
+    const id = req.params.userId;
+    User.findOne({ _id: id })
+        .select(selectFields)
+        .exec()
+        .then(user => {
+            const response = {
+                user: user
+            }
+            res.status(200).json(response);
+        })
+        .catch(error => { res.status(500).json({ message: `Unable to GET user of id '${id}'`, error: error }) })
+}
+
+
+exports.create_user = (req, res, next) => {
+    const user = new User({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+    });
+    user.save().then(user => {
+        const response = {
+            message: `Created user of id '${user._id}' successfully`,
+            user: user
+        }
+        res.status(201).json({response});
+    })
+    .catch(error => { res.status(500).json({ message: `Unable to get CREATE user of id '${id}'`, error: error }) })
+}
+
+exports.delete_user = (req, res, next) => {
+    const id = req.params.userId;
+    User.findOneAndDelete({ _id: id })
+        .select(selectFields)
+        .exec()
+        .then(user => {
+            const response = {
+                message: `Deleted user of id '${user._id}' successfully`
+            }
+            res.status(200).json({ response })
+        })
+        .catch(error => { res.status(500).json({ message: `Unable to DELETE user of id '${id}'`, error: error }) })
 }
--- back/api/routes/users.js
+++ back/api/routes/users.js
@@ -5,5 +5,7 @@ const router = express.Router();
 const UsersController = require('../controllers/users');
 
 router.get('/', UsersController.get_all_users);
+router.get('/:userId', UsersController.get_user);
+router.post('/', UsersController.create_user);
 
 module.exports = router;
